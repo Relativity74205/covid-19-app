6bdb950fbdab2e194c5e4b681cd35097977d9469
Relativity74205 <arkadius@schuchhardt.com>
2020-04-06 22:35:08 +0200

    added odds_ratio_calculation

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from plotly.subplots import make_subplots
 
 import data_functions as dfunc
 import plot_functions as pfunc
+import odds_ratio_functions as orf
 
 
 def plot(df: pd.DataFrame, base_metric: str, flag_delta: bool, rolling_window: int):
@@ -43,22 +44,47 @@ def get_data(value_name: str, min_cases: int, rolling_window: int):
     return df
 
 
-pfunc.streamlit_max_width()
+def main_statistics():
+    st.sidebar.title("What to do")
+    metric_input = st.sidebar.selectbox('Select metric', ('cases', 'delta_cases', 'deaths', 'delta_deaths'))
+    min_cases_input = st.sidebar.number_input('min_cases', min_value=0, value=1000, format='%d')
+    rolling_window_input = st.sidebar.number_input('rolling_window', min_value=1, value=3, format='%d')
 
-st.sidebar.title("What to do")
-metric_input = st.sidebar.selectbox('Select metric', ('cases', 'delta_cases', 'deaths', 'delta_deaths'))
-min_cases_input = st.sidebar.number_input('min_cases', min_value=0, value=1000, format='%d')
-rolling_window_input = st.sidebar.number_input('rolling_window', min_value=1, value=3, format='%d')
+    if 'cases' in metric_input:
+        base_metric = 'cases'
+    else:
+        base_metric = 'deaths'
 
-if 'cases' in metric_input:
-    base_metric = 'cases'
-else:
-    base_metric = 'deaths'
+    df = get_data(base_metric, min_cases_input, rolling_window_input)
+    if 'delta' in metric_input:
+        generated_plot = plot(df, base_metric, True, rolling_window_input)
+    else:
+        generated_plot = plot(df, base_metric, False, rolling_window_input)
 
-df = get_data(base_metric, min_cases_input, rolling_window_input)
-if 'delta' in metric_input:
-    generated_plot = plot(df, base_metric, True, rolling_window_input)
-else:
-    generated_plot = plot(df, base_metric, False, rolling_window_input)
+    st.plotly_chart(generated_plot)
 
-st.plotly_chart(generated_plot)
+
+def main_odds_ratios():
+    st.sidebar.header('Choose parameters')
+    age: int = st.sidebar.number_input('Age', min_value=0, value=20)
+    heart: bool = st.sidebar.checkbox('Coronary heart diseases')
+    sofa: bool = st.sidebar.checkbox('SOFA score')
+    lymphocyte: int = st.sidebar.number_input('Lymphocyte count (× 10 9 /L)', min_value=0, value=0)
+    d_dimer: str = st.sidebar.selectbox('D-dimer (microgramm/L)', ('smaller 0.5', 'bigger 0.5', 'bigger 1.0'))
+    odds_ratio, odds_ratio_lower_ci, odds_ratio_upper_ci = orf.calc_odds_ratio(age, heart, sofa, lymphocyte, d_dimer)
+
+    st.write(f'Estimated odds_ratio for the settings is {odds_ratio:.2f}, '
+             f'CI ({odds_ratio_lower_ci:.2f}-{odds_ratio_upper_ci:.2f}).')
+
+
+def main():
+    mode: str = st.sidebar.selectbox('App mode', ('Numbers', 'Odds'))
+    pfunc.streamlit_max_width()
+    if mode == 'Numbers':
+        main_statistics()
+    else:
+        main_odds_ratios()
+
+
+if __name__ == '__main__':
+    main()
---
+++ odds_ratio_functions.py
@@ -0,0 +1,62 @@
+from typing import Dict
+
+import numpy as np
+import pandas as pd
+
+import config_odds_ratio
+
+
+def create_df_from_dict() -> pd.DataFrame:
+    d: Dict = config_odds_ratio.analyses_b_estimates
+    df = pd.DataFrame(d).transpose()
+    df['estimate_lower_ci'] = df['estimate'] - 2 * df['se']
+    df['estimate_upper_ci'] = df['estimate'] + 2 * df['se']
+    df['estimate_exp'] = np.exp(df['estimate'])
+    df['estimate_lower_ci_exp'] = np.exp(df['estimate_lower_ci'])
+    df['estimate_upper_ci_exp'] = np.exp(df['estimate_upper_ci'])
+
+    return df
+
+
+def calc_odds_ratio(age: int, heart: bool, sofa: bool, lymphocyte: int, d_dimer: str):
+    df: pd.DataFrame = create_df_from_dict()
+    if d_dimer == 'bigger 0.5':
+        df_dimer_middle = True
+        df_dimer_high = False
+    elif d_dimer == 'bigger 1.0':
+        df_dimer_middle = False
+        df_dimer_high = True
+    else:
+        df_dimer_middle = False
+        df_dimer_high = False
+
+
+    add_intercept = False
+    estimate = (df.loc['intercept', 'estimate'] * add_intercept +
+                age * df.loc['age', 'estimate'] +
+                heart * df.loc['heart', 'estimate'] +
+                sofa * df.loc['sofa', 'estimate'] +
+                lymphocyte * df.loc['lymphocyte', 'estimate'] +
+                df_dimer_middle * df.loc['d_dimer_middle', 'estimate'] +
+                df_dimer_high * df.loc['d_dimer_high', 'estimate']
+                )
+
+    estimate_lower_ci = (df.loc['intercept', 'estimate_lower_ci'] * add_intercept +
+                         age * df.loc['age', 'estimate_lower_ci'] +
+                         heart * df.loc['heart', 'estimate_lower_ci'] +
+                         sofa * df.loc['sofa', 'estimate_lower_ci'] +
+                         lymphocyte * df.loc['lymphocyte', 'estimate_lower_ci'] +
+                         df_dimer_middle * df.loc['d_dimer_middle', 'estimate_lower_ci'] +
+                         df_dimer_high * df.loc['d_dimer_high', 'estimate_lower_ci']
+                         )
+
+    estimate_upper_ci = (df.loc['intercept', 'estimate_upper_ci'] * add_intercept +
+                         age * df.loc['age', 'estimate_upper_ci'] +
+                         heart * df.loc['heart', 'estimate_upper_ci'] +
+                         sofa * df.loc['sofa', 'estimate_upper_ci'] +
+                         lymphocyte * df.loc['lymphocyte', 'estimate_upper_ci'] +
+                         df_dimer_middle * df.loc['d_dimer_middle', 'estimate_upper_ci'] +
+                         df_dimer_high * df.loc['d_dimer_high', 'estimate_upper_ci']
+                         )
+
+    return np.exp(estimate), np.exp(estimate_lower_ci), np.exp(estimate_upper_ci)
